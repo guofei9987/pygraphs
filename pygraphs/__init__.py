ba97611000ade203ea70283eebeaa856de770655
guofei <guofei9987@foxmail.com>
2020-12-05 17:11:46 +0800

    docs

--- pygraphs/__init__.py
+++ pygraphs/__init__.py
@@ -1,2 +1,15 @@
 from .pygraphs_base import *
 from .tools import load_db, save_db
+
+__version__ = '0.0.1'
+
+
+def start():
+    print('''
+    pygraphs import successfully,
+    version: {version}
+    Author: Guo Fei,
+    Email: guofei9987@foxmail.com
+    repo: https://github.com/guofei9987/pygraphs,
+    documents: https://github.com/guofei9987/pygraphs
+    '''.format(version=__version__))
